237045bdb9a39c3793d67a4f4c13fd7ba5f6588c
puggo <puggoo1145@gmail.com>
2024-08-17 15:56:30 +0800

    增加页面切换加载进度条

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 // components
 import MyDock from "@/components/my-dock";
+import NprogressProvider from "@/components/providers/nprogress-provider";
 // utils
 import { cn } from "@/lib/utils";
 
@@ -14,7 +15,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "Course Recommander",
   description: "找到最适合你的学习方式",
 };
 
@@ -25,17 +26,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
+      <body
         className={cn(inter.className, "w-screen h-screen flex flex-col items-center")}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-        >
-          {children}
-          <MyDock />
-        </ThemeProvider>
+        <NprogressProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            {children}
+            <MyDock />
+          </ThemeProvider>
+        </NprogressProvider>
       </body>
     </html>
   );
